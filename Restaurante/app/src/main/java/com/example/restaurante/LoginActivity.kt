eb4d2702d3137452e1696b92732619cf166d84c9
CrisPereira <160641906+CrisOliveira0@users.noreply.github.com>
2024-05-21 14:24:10 -0300

    LoginActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
@@ -8,29 +8,34 @@ import com.example.restaurante.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 //Criação da variável binding que servirá para manipular a tela.
+
     private lateinit var binding:ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //a variável binding interliga o layout ao código
         binding = ActivityLoginBinding.inflate(layoutInflater)
+        //mostra o layout
         setContentView(binding.root)
 
+        //cria um evento que entrará em vigor quando o usuário clicar no botão de entrar
         binding.buttonEntrar.setOnClickListener {
+            //cria dois valore que vão receber os dados que o cliente digitar (nome e senha)
             val username = binding.editUsername.text.toString().trim()
             val password = binding.editPassword.text.toString().trim()
 
-            if (username.equals("abc") && password.equals("123")) {
+            //login
+            if (username.equals("Cristal Pereira de Oliveira") && password.equals("05")) {
                 val i = Intent(this, MainActivity::class.java)
                 i.putExtra("username", username)
+                //chama função "startActivity" passando o valor da variavel "i"
                 startActivity(i)
                 finish()
             } else {
+                //Mensagem de erro
                 Toast.makeText(applicationContext, "Errou", Toast.LENGTH_LONG).show()
             }
         }
 
-    }
-}
     }
 }
